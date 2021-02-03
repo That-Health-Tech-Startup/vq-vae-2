977961158b6fe9ea9cff845811343ae0c3bbb510
danielbang907 <danielbang907@gmail.com>
2021-02-03 06:01:52 -0600

    modified code to match labels to latent space

--- vq_vae_2/examples/mnist/train_generator.py
+++ vq_vae_2/examples/mnist/train_generator.py
@@ -1,7 +1,7 @@
 """
 Train a PixelCNN on MNIST using a pre-trained VQ-VAE.
 """
-
+import pandas as pd
 import os, sys
 sys.path.append("../")
 sys.path.append("../../")
@@ -14,7 +14,7 @@ import torchvision.transforms
 
 from vq_vae_2.examples.mnist.model import Generator, make_vq_vae
 
-BATCH_SIZE = 32
+BATCH_SIZE = 60000
 LR = 1e-3
 DEVICE = torch.device('cpu')
 
@@ -25,17 +25,27 @@ def main():
     vae.to(DEVICE)
     vae.eval()
 
-    test_images = load_images(train=False)
+    test_images = load_images(train=True)
+    
+    
+    
+
     for batch_idx, images in enumerate(load_images()):
-        images = images.to(DEVICE)
-        losses = []
-        for img_set in [images, next(test_images).to(DEVICE)]:
-            print(vae.encoders)
+        labels = images[1]
+        images = images[0].to(DEVICE)
+        print(labels)
+        for img_set in [images, next(test_images)[0].to(DEVICE)]:
             _, _, encoded = vae.encoders[0](img_set)
             print(encoded.shape)
+            print(encoded)
+            dic = {"latent space representation" : [f"{x}" for x in encoded], "labels" : labels}
+            df = pd.DataFrame(data=dic)
+            df.to_csv("MNISTLatent.csv")
             break
+        
         break
 
+        
 
 
 
@@ -43,14 +53,14 @@ def main():
 
 def load_images(train=True):
     while True:
-        for data, _ in create_data_loader(train):
-            yield data
+        for data, label in create_data_loader(train):
+            yield (data, label)
 
 
 def create_data_loader(train):
     mnist = torchvision.datasets.MNIST('./data', train=train, download=True,
                                        transform=torchvision.transforms.ToTensor())
-    return torch.utils.data.DataLoader(mnist, batch_size=BATCH_SIZE, shuffle=True)
+    return torch.utils.data.DataLoader(mnist, batch_size=BATCH_SIZE, shuffle=False)
 
 
 if __name__ == '__main__':
