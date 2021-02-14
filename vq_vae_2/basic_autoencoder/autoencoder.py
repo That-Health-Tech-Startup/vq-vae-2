2f69b043c3659ec775ded86067131fbd9089ecb2
Albert Kim <albertkim1234@gmail.com>
2021-02-14 16:57:41 -0600

    working autoencoder!

--- vq_vae_2/basic_autoencoder/autoencoder.py
+++ vq_vae_2/basic_autoencoder/autoencoder.py
@@ -7,14 +7,14 @@ class AutoEncoder(nn.Module):
     def __init__(self, in_channels, out_channels):
         super().__init__()
         self.encoder = nn.Sequential(
-                        nn.Conv2d(in_channels, out_channels, kernel_size = 3, stride = 2),
+                        nn.Conv2d(in_channels, out_channels, kernel_size = 3, stride = 2, padding = 1),
                         nn.ELU(),
-                        nn.Conv2d(out_channels, out_channels, kernel_size = 3, stride = 2),
+                        nn.Conv2d(out_channels, out_channels, kernel_size = 3, stride = 2, padding = 1),
                         nn.ELU())
         self.decoder = nn.Sequential(
-                        nn.ConvTranspose2d(out_channels, out_channels, stride = 2, kernel_size = 2, padding = 1),
+                        nn.ConvTranspose2d(out_channels, out_channels, stride = 2, kernel_size = 4, padding = 1),
                         nn.ELU(),
-                        nn.ConvTranspose2d(out_channels, in_channels, stride = 2, kernel_size = 2, padding = 1),
+                        nn.ConvTranspose2d(out_channels, in_channels, stride = 2, kernel_size = 4, padding = 1),
                         nn.ELU())
         
     def forward(self, x):
--- vq_vae_2/basic_autoencoder/train_ae.py
+++ vq_vae_2/basic_autoencoder/train_ae.py
@@ -1,7 +1,6 @@
-import torch.cuda
-import torch.optim
+import torch
 import torchvision.datasets
-import torch.utils.data
+import matplotlib as plt
 from autoencoder import AutoEncoder
 
 def main():
@@ -12,11 +11,26 @@ def main():
     # pull one batch for testing autoencoder
     test_batch = next(iter(mnist_dataloaders))
     
+    # instantiate autoencoder model and adam optimizer
     model = AutoEncoder(1, 5)
     param = model.parameters()
     optimizer = torch.optim.Adam(param)
+    loss_func = torch.nn.MSELoss(reduction = 'sum')
+    losses = []
     
-
+    # 250 epochs
+    for i in range(300):
+        recon = model(test_batch[0])
+        loss = loss_func(recon, test_batch[0])
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        losses.append(loss)
+    
+    rand_batch = next(iter(mnist_dataloaders))
+    plt.pyplot.imshow(model(rand_batch[0][1].unsqueeze(0)).detach().numpy().squeeze())
+    plt.pyplot.imshow(rand_batch[0][1].detach().numpy().squeeze())    
+    
     
 if __name__ == "__main__":
     main()
